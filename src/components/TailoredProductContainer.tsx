0ba796fff15585c57268b992df9c86e4aa4b89db
HowieG <howardbgil@gmail.com>
2023-07-05 02:01:04 -0700

    Added accordion and pretty hover effects for DescriptionContainer. Lots of fiddling with Mui

--- src/components/TailoredProductContainer.tsx
+++ src/components/TailoredProductContainer.tsx
@@ -7,6 +7,16 @@ import { stringToDescriptionTokenArray } from "~/utils";
 import { testDescriptionTokens } from "~/data/test-data";
 import DescriptionComponent from "./DescriptionComponent";
 
+import { styled } from "@mui/material/styles";
+import ArrowForwardIosSharpIcon from "@mui/icons-material/ArrowForwardIosSharp";
+import MuiAccordion, { AccordionProps } from "@mui/material/Accordion";
+import MuiAccordionSummary, {
+  AccordionSummaryProps,
+} from "@mui/material/AccordionSummary";
+import MuiAccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
+import { useEffect } from "react";
+
 const imageStyles = {
   display: "flex",
   width: "100%",
@@ -20,32 +30,78 @@ export default function TailoredProductContainer({
 }: {
   data: TailoredProduct[];
 }) {
+  const [expanded, setExpanded] = React.useState<string | boolean>(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setExpanded(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <Box>
       <Masonry columns={4} spacing={2}>
         {data.map((item, index) => (
           <div key={index}>
-            <Image
-              src={item.product.imgUrl}
-              alt="Alt Text"
-              loading="lazy"
-              style={imageStyles}
-              width="0"
-              height="0"
-              sizes="100vw"
-              className="h-auto w-full"
-            />
-            <DescriptionComponent
-              descriptionTokens={stringToDescriptionTokenArray(
-                testDescriptionTokens
-              )}
-            />
+            <Accordion
+              defaultExpanded={true}
+              expanded={expanded == true || expanded === `panel${index + 1}`}
+              onMouseEnter={() => setExpanded(`panel${index + 1}`)}
+              onMouseLeave={() => setExpanded(false)}
+              square // No idea how this fixes things but it does
+              sx={{ borderRadius: "16px", background: "transparent" }} // TODO: Find somewhere more appropriate to put this
+            >
+              <AccordionSummary
+                aria-controls={`panel${index + 1}d-content`}
+                id={`panel${index + 1}d-header`}
+              >
+                <Image
+                  src={item.product.imgUrl}
+                  alt="Alt Text"
+                  loading="lazy"
+                  style={imageStyles}
+                  width="0"
+                  height="0"
+                  sizes="100vw"
+                  className="h-auto w-full"
+                />
+              </AccordionSummary>
+              <AccordionDetails>
+                <DescriptionComponent
+                  descriptionTokens={stringToDescriptionTokenArray(
+                    testDescriptionTokens
+                  )}
+                />
+              </AccordionDetails>
+            </Accordion>
           </div>
         ))}
       </Masonry>
     </Box>
   );
 }
+
+const Accordion = styled((props: AccordionProps) => (
+  <MuiAccordion disableGutters elevation={16} {...props} />
+))(({ theme }) => ({
+  padding: 0,
+  backgroundColor: "transparent",
+}));
+
+const AccordionSummary = styled((props: AccordionSummaryProps) => (
+  <MuiAccordionSummary {...props} />
+))(({ theme }) => ({
+  padding: 0,
+  "& .MuiAccordionSummary-content": {
+    margin: 0,
+  },
+}));
+
+const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
+  padding: 0,
+}));
 
 // const Wrapper = () => {
 // 	return (
