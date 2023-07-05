27b47908f8bc9a86281b28fa1285be4900d65d90
HowieG <howardbgil@gmail.com>
2023-07-05 03:53:34 -0700

    File was missing from last commit

--- src/components/SourceProductContainer.tsx
+++ src/components/SourceProductContainer.tsx
@@ -2,9 +2,7 @@ import React from "react";
 import Image from "next/image";
 
 import Box from "@mui/material/Box";
-import Paper from "@mui/material/Paper";
-import { styled } from "@mui/material/styles";
-import type { ReferenceProduct, Product } from "~/types/TailorTypes";
+import type { ReferenceProduct } from "~/types/TailorTypes";
 import DescriptionComponent from "./DescriptionComponent";
 import { stringToDescriptionTokenArray } from "~/utils";
 import { testDescriptionTokens } from "~/data/test-data";
@@ -16,16 +14,6 @@ const imageStyles = {
   borderRadius: "16px",
   objectFit: "cover" as const,
 };
-
-const Label = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-  ...theme.typography.body2,
-  padding: theme.spacing(0.5),
-  textAlign: "center",
-  color: theme.palette.text.secondary,
-  borderBottomLeftRadius: 0,
-  borderBottomRightRadius: 0,
-}));
 
 export default function SourceProductContainer({
   data,
@@ -56,34 +44,3 @@ export default function SourceProductContainer({
     </Box>
   );
 }
-
-// const Wrapper = () => {
-//   return (
-//     <div style={{ display: "inline-flex", padding: "8px" }}>
-//       <Container></Container>
-//     </div>
-//   );
-// };
-
-// interface ContainerProps {
-//   children: ReactNode;
-// }
-
-// const Container: React.FC<ContainerProps> = ({ children }) => {
-//   return (
-//     <div
-//       style={{
-//         display: "flex",
-//         alignItems: "middle",
-//         boxSizing: "border-box",
-//         cursor: "pointer",
-//         width: "236px",
-//         position: "sticky",
-//         top: 0,
-//         zIndex: 1,
-//       }}
-//     >
-//       {children}
-//     </div>
-//   );
-// };
