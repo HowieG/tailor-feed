abe80a9f5b5065a3394a36a9abc45a7a2559f919
HowieG <howardbgil@gmail.com>
2023-07-11 02:48:40 -0700

    Removed extraneous lines

--- src/pages/demo.tsx
+++ src/pages/demo.tsx
@@ -1,16 +1,11 @@
 import { useEffect } from "react";
-import { useRouter } from "next/router";
-
 const Demo = () => {
-  const router = useRouter();
-
   useEffect(() => {
-    // Redirect to www.google.com
     window.location.href =
       "https://www.loom.com/share/cf72ee681b5d4b1481385cc425f2cdf7?sid=da5c30c2-a971-4a2e-a515-5178a705ac9d";
   }, []);
 
-  return null; // or you can render a loading indicator or custom message
+  return null;
 };
 
 export default Demo;
